4656bdf4e36dc765e04aa272525f19835c1fbdd7
misskaori <liuweizhe@hotmail.com>
2020-07-23 13:45:33 +0800

    Change Test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -12,7 +12,7 @@ class DemoApplicationTests {
     @Test
     void contextLoads() {
         System.out.println(mapper.select().get(3).getName());
-        System.out.println("HAHA");
+        System.out.println("nani?");
     }
 
 }
