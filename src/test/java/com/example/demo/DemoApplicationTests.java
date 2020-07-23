a2b1873de8d42a6d5396e0ea2bfd038a46c12842
misskaori <liuweizhe@hotmail.com>
2020-07-23 10:58:01 +0800

    Change Test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -12,7 +12,7 @@ class DemoApplicationTests {
     @Test
     void contextLoads() {
         System.out.println(mapper.select().get(3).getName());
-        System.out.println("KAORI");
+        System.out.println("KARI");
     }
 
 }
